9aeb0f3f96dabe5d0bd665e75104bb1733ca803b
PRIDA-TAKON <takonaom@gmail.com>
2025-11-05 00:56:03 +0700

    fix api 1.2.82

--- assets/js/api-config.js
+++ assets/js/api-config.js
@@ -1,3 +1,3 @@
-const API_BASE_URL = 'http://localhost:8181'; // Default for local development
-
+// API_BASE_URL will be defined by Webpack's DefinePlugin
+// during the build process based on process.env.API_BASE_URL
 export default API_BASE_URL;
