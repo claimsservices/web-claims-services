eacf03128a2d4b303e9bdf28562816cc9d011ee9
PRIDA-TAKON <takonaom@gmail.com>
2026-01-28 02:52:09 +0700

    debug time stamp 1.3.207

--- assets/js/task-management-refactored.js
+++ assets/js/task-management-refactored.js
@@ -773,7 +773,7 @@ document.addEventListener('DOMContentLoaded', function () {
         channel: getValueById('channel'),
         process_type: getValueById('processType'),
         insur_comp: getValueById('insuranceCompany'),
-        order_date: getValueById('transactionDate'),
+        order_date: new Date().toISOString(),
         appointment_date: appointment_date,
         car_registration: getValueById('carRegistration'),
         location: getValueById('address'),
